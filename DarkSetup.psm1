d3e0f84d098dd8de587729d8713c1f169b7d9aa1
Dessel Bane <dessel.bane@gmx.de>
2023-09-27 14:30:18 +0200

    Add basic programm install Add font install Add pwsh profile extension

---
+++ DarkSetup.psm1
@@ -0,0 +1,93 @@
+Import-Module ps-menu
+
+function Install-Font {
+    param(
+        [string]
+        $fontDir
+    )
+
+    Write-Host "Installing fonts of folder $fontDir"
+    $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    Get-ChildItem $fontDir `
+    | Where-Object { $_.Name -match "(t|o)tf$" } `
+    | ForEach-Object { Write-Host "installing $_" <# $fonts.CopyHere($_.FullName) #> }
+    
+}
+Export-ModuleMember -Function Install-Font
+
+function Install-Programm {
+    param (
+        [string]
+        $programmId
+    )
+    Write-Output "winget install $programmId --accept-package-agreements --accept-source-agreements"
+}
+Export-ModuleMember -Function Install-Programm
+
+function Handle-Fonts {
+    param(
+        $configItem
+    )
+    $id = $configItem.Id
+
+
+    for ($ii = 0; $ii -lt $configItem.Fonts.Length; $ii++) {
+        $outFolder = "$id.$ii"
+        $outName = "$outFolder.zip"
+
+        Write-Output "Invoke-WebRequest $configItem.Fonts[$ii] -OutFile $outName"
+        Write-Output "Expand-Archive $outName"
+        Write-Output "Install-Font -fontDir $outFolder"
+
+        Write-Output "Remove-Item -Recurse -Force $outFolder"
+        Write-Output "Remove-Item -Force $outName"
+    }
+}
+
+function Handle-Profile {
+    param (
+        $configItem
+    )
+
+    if($null -eq $configItem.PwshProfile){
+        return
+    }
+
+    $exists = [System.IO.File]::Exists($PROFILE)
+
+    if ( -Not $exists) {
+        Write-Host "Creating Pwsh Profile: $PROFILE"
+        New-Item -Path $PROFILE -ItemType File -Force
+    }
+
+    $profileContainsText = Select-String -Path $PROFILE -Pattern "$($configItem.PwshProfile)" -SimpleMatch
+
+    if ($null -eq $profileContainsText){
+        Write-Host "Appending '$($configItem.PwshProfile)' to pwsh profile"
+        Add-Content -Path $PROFILE -Value $configItem.PwshProfile
+    } else {
+        Write-Host "'$($configItem.PwshProfile)' is already part of pwsh profile"
+    }
+    
+}
+
+function SetupFromConfig {
+    param (
+        [string]
+        $configPath
+    )
+    $config = Get-Content -Path $configPath | ConvertFrom-Json
+
+    $menuChoices = $config | Select-Object -ExpandProperty Name
+    $options = Menu $menuChoices -ReturnIndex -Multiselect 
+
+    foreach ($i in $options) {
+        $configItem = $config[$i]
+
+        Install-Programm -programmId $configItem.Id
+
+        Handle-Fonts -configItem $configItem
+        Handle-Profile -configItem $configItem
+    }
+}
+Export-ModuleMember -Function SetupFromConfig
