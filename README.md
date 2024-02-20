067d721473295cdade58eb635a567152ce523829
Dessel Bane <desselbane999+github@gmail.com>
2024-02-20 11:33:12 +0100

    add bootstrap script

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup
+
+Bootstrap a new windows.
+
+```pwsh
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/desselbane/setup/master/bootstrap.ps1 -OutFile .\bootstrap.dl.ps1 && & .\bootstrap.dl.ps1
+```
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,1 +1,54 @@
-Write-Host "Hello World from bootstrap"
+function New-TemporaryDirectory {
+  $parent = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+
+Write-Host "Bootstraping Windows, hold on to your socks"
+
+$tempDir = Join-Path $([System.IO.Path]::GetTempPath()) $([System.Guid]::NewGuid())
+New-Item -ItemType Directory -Path $tempDir
+
+try {
+  Set-Location $tempDir
+
+  Write-Host "Checking for WinGet"
+  $wingetVersion = winget -v
+
+  if (-not $wingetVersion.StartsWith("v")) {
+    Write-Host "Did not find Winget. Installing..."
+
+    Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.6.3482/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget-setup.msixbundle
+
+    & ./winget-setup.msixbundle
+  }
+
+  $wingetVersion = winget -v
+
+  if (-not $wingetVersion.StartsWith("v")) {
+    Write-Host "Winget installation failed. Exiting..."
+
+    Return -1
+  }
+
+  Write-Host "Installing git"
+  winget install Git.Git
+
+  Write-Host "Cloning repo"
+  git clone https://github.com/DesselBane/setup.git
+  Set-Location setup
+
+  Write-Host "Installing InteractiveMenu"
+  Install-Module InteractiveMenu
+
+  Write-Host "Importing Module DarkSetup"
+  Import-Module .\DarkSetup.psm1
+
+  Write-Host "Running Setup"
+  SetupFromConfig .\program.config.json
+}
+finally {
+  Write-Host "Removing temp dir"
+  Remove-Item -Path $tempDir -Recurse -Force
+}
